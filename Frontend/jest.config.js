6f4c067c0d4b4783489d2c3b54b9fb45a2fd60e0
Lakkitha <lakkithachamath420@gmail.com>
2025-02-16 22:10:38 +0530

    Jest configuration to generate coverage reports

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -16,4 +16,12 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.tsx',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
 };
