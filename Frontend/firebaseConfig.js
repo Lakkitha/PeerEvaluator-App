7be390835e84e2dd9e4c22583df71e7ec37f60b7
Lakkitha <lakkithachamath420@gmail.com>
2025-03-03 21:59:01 +0530

    Added some more changes to the firebase config

--- Frontend/firebaseConfig.js
+++ Frontend/firebaseConfig.js
@@ -4,16 +4,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB66-873mizJ1rLhoN0HUM0t2B72v-EZGg',
-  authDomain: '637568203533-3epjfnrskepquq0i1mrpjv0f3q4rig5t.apps.googleusercontent.com',
+  authDomain: 'peerevaluator-app.firebaseapp.com',
   projectId: 'peerevaluator-app',
-  storageBucket: '"peerevaluator-app.firebasestorage.app',
-  messagingSenderId: '1:637568203533:android:467bd133c9199af50a8e44',
-  appId: '637568203533-micdr3ramm47f4s299tiro7t2ntm26cv.apps.googleusercontent.com',
+  storageBucket: 'peerevaluator-app.firebasestorage.app',
+  messagingSenderId: '637568203533',
+  appId: '1:637568203533:android:467bd133c9199af50a8e44',
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
 
 export {auth};
